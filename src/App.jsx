b8958690341a67f30dba839e736a557bd31bd020
nav2103 <navneetsingh2103@gmail.com>
2024-07-22 15:10:28 +0530

    final commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,11 @@
 import "./App.css";
 import TicTacToe from "./tictactoe/TicTacToe";
+import Footer from "./components/Footer/Footer";
 function App() {
   return (
     <>
       <TicTacToe />
+      <Footer />
     </>
   );
 }
---
+++ src/components/Footer/Footer.jsx
@@ -0,0 +1,16 @@
+import "./footer.css";
+
+const Footer = () => {
+  return (
+    <>
+      <footer className="footer">
+        <p>
+          Developed with &hearts; by{" "}
+          <a href="https://github.com/nav2103">Navneet</a>
+        </p>
+      </footer>
+    </>
+  );
+};
+
+export default Footer;
